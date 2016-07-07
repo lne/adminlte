8b89a632925e17b1714d989ad0d1f5fb3b223668
wei <dongfeng.wei.du@hitachi-solutions.com>
2016-07-07 09:36:32 +0900

    deleted nouse css/js

--- vendor/assets/javascripts/adminlte.js
+++ vendor/assets/javascripts/adminlte.js
@@ -3,10 +3,10 @@
 //= require adminlte/plugins/bootstrap-slider/bootstrap-slider
 //= require adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min
 // ChartJS: 
-//= require adminlte/plugins/chartjs/Chart.min
-//= require adminlte/plugins/colorpicker/bootstrap-colorpicker.min
+// require adminlte/plugins/chartjs/Chart.min
+// require adminlte/plugins/colorpicker/bootstrap-colorpicker.min
 //= require adminlte/plugins/datatables/jquery.dataTables.min
-//= require adminlte/plugins/datatables/dataTables.bootstrap.min
+// require adminlte/plugins/datatables/dataTables.bootstrap.min
 //= require adminlte/plugins/datepicker/bootstrap-datepicker
 //= require adminlte/plugins/daterangepicker/moment.min
 //= require adminlte/plugins/daterangepicker/daterangepicker
@@ -36,13 +36,13 @@
 // require adminlte/plugins/fullcalendar/fullcalendar.min
 //
 // iCheck: Highly customizable checkboxes and radio buttons for jQuery and Zepto.
-//= require adminlte/plugins/iCheck/icheck.min
+// require adminlte/plugins/iCheck/icheck.min
 //
 // jquery.inputmask: A jQuery Plugin to make masks on form fields and html elements.
-//= require_tree ./adminlte/plugins/input-mask/.
+// require_tree ./adminlte/plugins/input-mask/.
 //
 // Ion.RangeSlider: cool, comfortable, responsive and easily customizable range slider.
-//= require adminlte/plugins/ionslider/ion.rangeSlider.min
+// require adminlte/plugins/ionslider/ion.rangeSlider.min
 //
 // jQuery UI
 //= require adminlte/plugins/jQueryUI/jquery-ui.min
@@ -51,23 +51,23 @@
 // require_tree ./adminlte/plugins/jvectormap/.
 //
 // jQuery Knob: Nice, downward compatible, touchable, jQuery dial.
-//= require adminlte/plugins/knob/jquery.knob
+// require adminlte/plugins/knob/jquery.knob
 //
 // morris.js: Draw charts.
-//= require adminlte/plugins/morris/morris.min
+// require adminlte/plugins/morris/morris.min
 //
 // Pace: Get a beautiful progress indicator for your page load and ajax navigation.
-//= require adminlte/plugins/pace/pace.min
+// require adminlte/plugins/pace/pace.min
 //
 // Select2: The jQuery replacement for select boxes.
-//= require adminlte/plugins/select2/select2.full.min
+// require adminlte/plugins/select2/select2.full.min
 //
 // jQuery slimScroll: Transforms any div into a scrollable area with a nice scrollbar.
-//= require adminlte/plugins/slimScroll/jquery.slimscroll.min
+// require adminlte/plugins/slimScroll/jquery.slimscroll.min
 //
 // SPARKLINE CHARTS: Create a inline charts with spark line.
-//= require adminlte/plugins/sparkline/jquery.sparkline.min
+// require adminlte/plugins/sparkline/jquery.sparkline.min
 //
 // TimePicker
-//= require adminlte/plugins/timepicker/bootstrap-timepicker.min
+// require adminlte/plugins/timepicker/bootstrap-timepicker.min
 //
